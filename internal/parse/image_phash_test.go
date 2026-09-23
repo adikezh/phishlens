2c87580b12825ffb7846ec3a4286941afe103512
a.tleukhan <a.tleukhan@aidia.kz>
2026-09-24 01:19:37 +0500

    test: verify image palette extraction

--- internal/parse/image_phash_test.go
+++ internal/parse/image_phash_test.go
@@ -31,3 +31,18 @@ func TestImageAddsPerceptualHash(t *testing.T) {
 	require.Len(t, mail.Images, 1)
 	require.NotEmpty(t, mail.Images[0].PHash)
 }
+
+func TestImageExtractsDominantColorPalette(t *testing.T) {
+	img := image.NewRGBA(image.Rect(0, 0, 32, 32))
+	for y := 0; y < 32; y++ {
+		for x := 0; x < 32; x++ {
+			img.Set(x, y, color.RGBA{R: 0xF1, G: 0x46, B: 0x35, A: 0xFF})
+		}
+	}
+	var data bytes.Buffer
+	require.NoError(t, png.Encode(&data, img))
+	mail, err := New().Image(context.Background(), data.Bytes())
+	require.ErrorIs(t, err, ErrOCRUnavailable)
+	require.Len(t, mail.Images, 1)
+	require.Equal(t, []string{"#FF4433"}, mail.Images[0].Colors)
+}
